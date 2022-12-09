e9a6da72c46800250cd354bd4ad3fb500a78bc3a
aprilslab <april.kim412@gmail.com>
2022-12-09 18:29:38 +0900

    Updated Tensorflow serving

--- TensorflowServing/test.py
+++ TensorflowServing/test.py
@@ -1,5 +1,7 @@
 import random, json
 import keras.datasets.fashion_mnist
+import requests
+import numpy as np
 
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
@@ -16,21 +18,25 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 # test 이미지 중에 임의의 위치 하나 선택하기
 rando = random.randint(0, len(test_images) - 1)
 
-data = json.dumps({"signature_name": "serving_default", "instances": test_images[0:3].tolist()})
+instances = np.expand_dims(test_images[rando], axis=0).tolist()
+data = json.dumps({"signature_name": "serving_default", "instances": instances})
 print('Data: {} ... {}'.format(data[:50], data[len(data)-52:]))
 
 #서버에 요청하기
-import requests
-import numpy as np
 headers = {'Content-Type': "application/json"}
 json_response = requests.post('http://localhost:8501/v1/models/test_model:predict', data=data, headers=headers)
-print(f'json response\n{json_response.text}')
-predictions = json.loads(json_response.text)['predictions']
+prediction = json.loads(json_response.text)['predictions'][0]
 
-for i in range(3):
-    print(('The model thought this was a {} (class {}), and it was actually a {} (class {})').format(
-        class_names[np.argmax(predictions[i])],
-        np.argmax(predictions[i]),
-        class_names[test_labels[i]],
-        test_labels[i]
+print(('The model thought this was a {} (class {}), and it was actually a {} (class {})').format(
+        class_names[np.argmax(prediction)],
+        np.argmax(prediction),
+        class_names[test_labels[rando]],
+        test_labels[rando]
     ))
+# for i in range(3):``
+#     print(('The model thought this was a {} (class {}), and it was actually a {} (class {})').format(
+#         class_names[np.argmax(predictions[i])],
+#         np.argmax(predictions[i]),
+#         class_names[test_labels[i]],
+#         test_labels[i]
+#     ))
